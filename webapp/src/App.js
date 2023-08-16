44b4cb6af7bc7d7a2761d404f60735ecc1ee1bd2
MuhammadZubair210 <mzubairabdullah210@gmail.com>
2023-08-16 03:49:58 -0300

    Environment test

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -127,6 +127,7 @@ function App() {
       <header className="App-header">
         <div className="logo-container">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
           <h1 className="text-logo">Intern.gg</h1>
         </div>
         <div className="content-container">
